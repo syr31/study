57e897b5e6be06d065d56d8e1def44e27416b5c4
yeoungrae shin <dudfo6425@gmail.com>
2022-11-12 17:14:34 +0900

    221112 입력된 값들 중 최대값 최소값 구하는 문제

--- important/src/important_2/quiz_2.java
+++ important/src/important_2/quiz_2.java
@@ -5,22 +5,20 @@ import java.util.Scanner;
 public class quiz_2 {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 
 		int inp = 0;
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
+		int max = 0;
+		int min = 10000;
 
 		for (int i = 0; i < 6; i++) {
 			inp = sc.nextInt();
 
 			max = max > inp ? max : inp;
 			min = min < inp ? min : inp;
-
-			System.out.printf("max : %d min : %d", max, min);
 		}
+		sc.close();
 
+		System.out.printf("max : %d min : %d", max, min);
 	}
-
 }
